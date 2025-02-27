9cfda70b8f7c4d1d37bea4cd396606ad2f083a55
Thewsthews <etiegnim@gmail.com>
2025-02-27 23:57:55 +0300

    Changes to index.js

--- react-pp/src/index.js
+++ react-pp/src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import{
+  algorithms,
+  oscs,
+  devnotes,
+} from "../assets";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+export const navLinks=[
+  {
+    id: "huntsman",
+    title: "Huntsman",
+  },
+  {
+    id:"points",
+    title: "Points",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const points = [
+  {
+    title:"Software Developer",
+    company:"Freelance",
+  }
+]
