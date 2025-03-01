72fa7fb12fdb9009beb8e0ef7774f984acdc42b9
Thewsthews <etiegnim@gmail.com>
2025-03-02 00:17:10 +0300

    Addition to index.js

--- react-pp/src/index.js
+++ react-pp/src/index.js
@@ -25,5 +25,5 @@ const points = [
     company:"Freelance",
     date: "2019 - Present",
     description: "I work as a freelance software developer specializing in backend development using Rust. I have worked on a variety of projects, including web development,  and desktop development.",
-  }
+  },
 ]
