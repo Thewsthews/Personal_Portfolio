fa0d9980473ab38726b1d7df8e238e07912ba3c7
Thewsthews <etiegnim@gmail.com>
2025-03-03 00:13:45 +0300

    Changes to index.js

--- react-pp/src/index.js
+++ react-pp/src/index.js
@@ -26,4 +26,10 @@ const points = [
     date: "2019 - Present",
     description: "I work as a freelance software developer specializing in backend development using Rust. I have worked on a variety of projects, including web development,  and desktop development.",
   },
+  {
+    title:"Junior Backend Developer",
+    company:"Freelance",
+    date: "2019 - Present",
+    description: "I work as a freelance software developer specializing in backend development using Ruby and later switched to Rust. I have worked on a variety of projects both in-house and opensource, including web development,  and desktop development.",
+  },
 ]
